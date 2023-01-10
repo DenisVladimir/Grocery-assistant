a9996f06ae9c6c484470aec4512eea61630bf7d8
DenisVladimir <git@github.com>
2023-01-10 22:24:56 +0300

    Commit8

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -8,7 +8,7 @@ load_dotenv()
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-SECRET_KEY = os.getenv("SECRET_KEY")
+SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', default=False, cast=bool)
